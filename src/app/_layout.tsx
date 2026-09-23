2c18f18123e5e5a02db5648c4adee7ab363c8621
Marcos Morales Mdz <marcos611morales@gmail.com>
2026-09-23 15:06:42 -0600

    refactor: remove unused components and simplify layout structure

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { Text, View } from 'react-native'
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import '../../global.css'
 
-import '../../global.css';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+	<View>
+	  <Text>RootLayout</Text>
+	</View>
+  )
 }
+
+export default RootLayout
