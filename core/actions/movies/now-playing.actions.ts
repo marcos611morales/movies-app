328930b2345daecf2988fbb9aa5b21d9796c199e
Marcos Morales Mdz <marcos611morales@gmail.com>
2026-09-23 17:55:23 -0600

    feat: implement movie interface and mapper, update now playing action

--- core/actions/movies/now-playing.actions.ts
+++ core/actions/movies/now-playing.actions.ts
@@ -1,13 +1,18 @@
 import { MovieDBMoviesResponse } from "../../../infrastructure/interfaces/movidedb-response";
+import { MovieMapper } from "../../../infrastructure/mappers/movie.mapper";
 import { movieApi } from "../../api/movie-api";
 
 
 export const nowPlayingAction = async() => {
 	try {
-		const {data} = await movieApi.get<MovieDBMoviesResponse>('/now_playing')
-		console.log(data);
+		const { data } = await movieApi.get<MovieDBMoviesResponse>('/now_playing')
+		// console.log(JSON.stringify(data, null, 2));
 
-		return [];
+		const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+		
+		console.log(movies)
+
+		return movies;
 	} catch (error) {
 		console.log(error);
 		throw 'Cannot load now playing movies'
---
+++ infrastructure/mappers/movie.mapper.ts
@@ -0,0 +1,18 @@
+import { Result } from "../interfaces/movidedb-response";
+import { Movie } from "../interfaces/movie.interface";
+
+
+
+export class MovieMapper {
+	static fromTheMovieDBToMovie = (movie: Result): Movie => {
+		return {
+			id: movie.id,
+			title: movie.title,
+			description: movie.overview,
+			releaseDate: new Date(movie.release_date),
+			poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+			backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+			rating: movie.vote_average
+		}
+	}
+}
